d6efc675b262c54e70cd7e974768ef13e0e4631d
Marko <stojkovmarko1@gmail.com>
2020-02-02 15:31:35 +0100

    Static Root

--- customer/customer/settings.py
+++ customer/customer/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 SEND_GRID_API_KEY = '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'
 EMAIL_HOST = 'smtp.sendgrid.net'
